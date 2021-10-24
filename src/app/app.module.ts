0edf2bbc3719b6a0420d54a2d4866afc1947379a
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2021-10-24 13:39:51 -0500

    The entire demo in one large commit...
    Diary commits are better ! ! !
    
    But they slow things down so I didn't use them for this demo : - (

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RollADieComponent } from './roll-a-die/roll-a-die.component';
+import { RollHistoryComponent } from './roll-history/roll-history.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RollADieComponent,
+    RollHistoryComponent,
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
